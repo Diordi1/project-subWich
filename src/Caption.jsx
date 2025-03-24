e5b930afd44a20f6e87db83dcf873834c1ca5718
wasteapp111@gmail.com <Diordi1>
2025-03-24 16:41:38 +0530

    color-burn

--- src/Caption.jsx
+++ src/Caption.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 function Caption() {
     return ( 
         <div className="bg-gray-200 p-4">
-            <p className="text-5xl font-bold text-gray-500">Made with 
-            <img className="ez-resource-show__preview__image h-4 w-4" alt="Heart vector design" fetchpriority="high" title="Heart vector design" draggable="false" data-zoom-src="https://static.vecteezy.com/system/resources/previews/027/875/990/large_2x/heart-design-free-vector.jpg" data-original-width="1920" data-original-height="1920" data-action="click->resource-show-preview-zoom#trackZoomIn" data-resource-show-preview-target="previewImage" data-image-zoom-target="image" src="https://static.vecteezy.com/system/resources/previews/027/875/990/large_2x/heart-design-free-vector.jpg" ></img>
+            <p className="text-5xl font-bold text-gray-500 flex flex-col">
+                <div>
+
+                Made with 
+            <img className="ez-resource-show__preview__image h-8 w-8 mix-blend-color-burn" alt="Heart vector design" fetchpriority="high" title="Heart vector design" draggable="false" data-zoom-src="https://static.vecteezy.com/system/resources/previews/027/875/990/large_2x/heart-design-free-vector.jpg" data-original-width="1920" data-original-height="1920" data-action="click->resource-show-preview-zoom#trackZoomIn" data-resource-show-preview-target="previewImage" data-image-zoom-target="image" src="https://static.vecteezy.com/system/resources/previews/027/875/990/large_2x/heart-design-free-vector.jpg" ></img>
+                </div>
                  in India</p>
-            <hr>
+            <hr className='text-gray-500 font-bold'>
             </hr>
-            <p className='font-semibold'>Diordel</p>
+            <p className='font-semibold text-gray-500 my-1'>Diordel</p>
         </div>
      );
 }
